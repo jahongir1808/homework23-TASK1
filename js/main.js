971f7ade340717eec46a79285abf9e09c3f76a8d
jahongir1808 <jahongir.developer18@gmail.com>
2023-01-18 17:21:01 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,79 @@
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+          if (this.status == 200) {
+            elmnt.innerHTML = this.responseText;
+          }
+          if (this.status == 404) {
+            elmnt.innerHTML = "Page not found.";
+          }
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      };
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
+    }
+  }
+}
+
+includeHTML();
+
+let backtop = document.getElementById("backtop");
+
+function toggleBacktop() {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    backtop.style.opacity = 1;
+  } else {
+    backtop.style.opacity = 0;
+  }
+}
+
+window.addEventListener("scroll", function () {
+  toggleBacktop();
+});
+
+function openNavbar() {
+  // document.getElementById("navbar-responsive").style.left = "0";
+  document.getElementById("navbar-responsive").style.top = "64px";
+}
+
+function closeNavbar() {
+  // document.getElementById("navbar-responsive").style.left = "-100%";
+  document.getElementById("navbar-responsive").style.top = "-200%";
+}
+
+window.addEventListener("scroll", function () {
+  shrink();
+});
+
+let navbar = document.getElementById("navbar");
+
+function shrink() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    navbar.classList.add("navbar-shrink");
+    // navbar.style.padding = "60px 0 15px";
+  } else {
+    navbar.classList.remove("navbar-shrink");
+    // navbar.style.padding = "73px 0 20px";
+  }
+}
